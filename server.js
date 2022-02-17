d88d35735ae3ba9af424900e44ead4b19be202c0
Rakesh <rbandi211570@gmail.com>
2022-02-17 15:43:57 -0800

    blah

--- server.js
+++ server.js
@@ -117,10 +117,10 @@ app.get('/confirmCode/:username/:registrationCode', (req, res) => {
                     const token = jwt.sign({ username: username }, secret);
                     User.updateOne({ _id: user._id }, { signatureSecret: secret }, (err, status) => {
                         if (err) res.json({ status: 'fail', reason: err })
-                        else res.cookie("intellisheets_token", token, {
+                        else res.cookie('intellisheets_token', token, {
                             httpOnly: true,
                             secure: process.env.NODE_ENV === "production",
-                        }).json({ status: 'success' });
+                        }).json({ status: 'success', context: 'cookie should be sent' });
                     });
                 }
                 else res.json({ status: 'fail', reason: 'invalid code', sigSecret: user.signatureSecret, regCode: registrationCode });
