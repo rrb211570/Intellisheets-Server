aac902bc7937299182152a8dd2ff35a8763eb8b4
Rakesh <rbandi211570@gmail.com>
2022-02-28 18:06:09 -0800

    set cookies expiration, so it does not get deleted on browser closing

--- server.js
+++ server.js
@@ -128,7 +128,8 @@ app.get('/confirmCode/:username/:registrationCode', (req, res) => {
                             res.cookie('access_token', access_token, {
                                 httpOnly: true,
                                 secure: true,
-                                sameSite: 'none'
+                                sameSite: 'none',
+                                expires: new Date(Date.now() + 1209600000)
                             });
                             res.json({ status: 'success', context: 'code confirmed' });
                         }
@@ -182,7 +183,8 @@ app.get('/login/:username/:password', (req, res) => {
                                         res.cookie('access_token', access_token, {
                                             httpOnly: true,
                                             secure: true,
-                                            sameSite: 'none'
+                                            sameSite: 'none',
+                                            expires: new Date(Date.now() + 1209600000)
                                         });
                                         res.json({ status: 'success', context: 'logged in' });
                                     }
